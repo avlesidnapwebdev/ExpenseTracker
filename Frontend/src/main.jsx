00c614c76bf796ae1ea30cdf25ead3c36052d739
KingRuby14 <selvapandi322@gmail.com>
2026-02-17 11:43:04 +0530

    update name

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./Index.css";
+import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.jsx"; // adjust path if needed
 
